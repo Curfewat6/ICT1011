1dbbbdd3b06c1a68ec8f9b16b0fec82709e662b1
Curfewat6 <75602956+Curfewat6@users.noreply.github.com>
2023-10-30 12:28:41 +0800

    Add files via upload
    added virtual pet
    disclaimer: the pet kinda whack . gonna fix the menu logic

---
+++ 30-10-2023/TestFunction.ino
@@ -0,0 +1,18 @@
+
+void nameTag(uint8_t buttons){
+  currentDisplayState = displayTimer;
+  if(!buttons){
+    displayBuffer.clearWindow(0, 8, 96, 64);
+    displayBuffer.fontColor(TS_16b_White, TS_16b_Black);
+    displayBuffer.setCursor(8, 15 - 6);
+    displayBuffer.print("Back");
+    displayBuffer.drawLine(1, 14,    1, 12, 0xFFFF);
+    displayBuffer.drawLine(1, 12,    6, 12, 0xFFFF);
+    backArrow();
+    
+  }else{
+    buttonStuff(backButton);
+  }
+
+  
+}
